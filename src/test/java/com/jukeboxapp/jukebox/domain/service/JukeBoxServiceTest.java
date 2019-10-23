0cfffa9395d4ebf7ae6d1a9dde50ed83703449db
algorithmikk <56777954+algorithmikk@users.noreply.github.com>
2019-10-22 23:36:05 -0400

    add mock and fix unit test dependencies

--- src/test/java/com/jukeboxapp/jukebox/domain/service/JukeBoxServiceTest.java
+++ src/test/java/com/jukeboxapp/jukebox/domain/service/JukeBoxServiceTest.java
@@ -1,35 +1,45 @@
 package com.jukeboxapp.jukebox.domain.service;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.support.PagedListHolder;
-
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import com.jukeboxapp.jukebox.api.rest.v1.ressource.Components;
 import com.jukeboxapp.jukebox.api.rest.v1.ressource.JukeBox;
+import com.jukeboxapp.jukebox.domain.client.JukeBoxClient;
+import com.jukeboxapp.jukebox.domain.mapper.JukeBoxMapper;
 
-import java.util.Optional;
-
+@SpringJUnitConfig
 public class JukeBoxServiceTest {
 
-	@InjectMocks
-	JukeBoxService service;
+	private JukeBoxService service;
+
+	private JukeBoxClient client;
+
+	private JukeBoxMapper mapper;
 
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
+		service = mock(JukeBoxServiceImpl.class);
+		client = mock(JukeBoxClient.class);
+		mapper = mock(JukeBoxMapper.class);
+
 	}
 
-	@Test
+	@org.junit.Test
 	public void getPaginatedListWithSettingIdandModelTest() {
 
 		List<JukeBox> listofJukes = new ArrayList<JukeBox>();
@@ -48,13 +58,18 @@ public class JukeBoxServiceTest {
 		listComp.add(comp);
 
 		juke.setComponents(listComp);
+		listofJukes.add(juke);
 
 		PagedListHolder<JukeBox> page = new PagedListHolder<JukeBox>(listofJukes);
 
+		doCallRealMethod().when(service).createPaginatedListOfJukes(Optional.of("5ca94a8acc046e7aa8040605"),
+				Optional.of("angelina"), Optional.of(0), Optional.of(10));
+
 		when(service.createPaginatedListOfJukes(Optional.of("5ca94a8acc046e7aa8040605"), Optional.of("angelina"),
-				Optional.of(0), Optional.of(10))).thenReturn(page);
+				Optional.of(0), Optional.of(10)).getPageList()).thenReturn(listofJukes);
 
 		assertEquals(1, page.getPageList().size());
+
 		verify(service, times(1)).createPaginatedListOfJukes(Optional.of("5ca94a8acc046e7aa8040605"),
 				Optional.of("angelina"), Optional.of(0), Optional.of(10));
 	}
